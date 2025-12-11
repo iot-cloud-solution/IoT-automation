b0befaebf15ef9eec27c40ea83a148ba57cad879
Bryan Avalos <bryanavalos23@outlook.com>
2025-12-10 22:32:15 -0600

    Cambios en process data function y db config function
    Se cambio tiempo de time out de lambda process data
    
    Se implemento consumo de secretos en db config function

--- src/db/db_config.py
+++ src/db/db_config.py
@@ -1,21 +1,47 @@
 import os
 import psycopg2
+import boto3
+import json
+
+def get_secrets():
+  secret_name = os.getenv("SECRET_NAME")
+  region = os.getenv("AWS_REGION")
+
+  client = boto3.client(
+    service_name='secretsmanager',
+    region_name=region
+  )
+
+  response = client.get_secret_value(
+    SecretId=secret_name
+  )
+
+  if 'SecretString' in response:
+    secret_dict = json.loads(response['SecretString'])
+  else:
+    secret_dict = json.loads(response['SecretBinary'])
+
+  return secret_dict
+
  
 def get_db_conexion():
   try:
-    print("HOST:", os.getenv("DB_HOST"))
-    print("DB:", os.getenv("DATABASE"))
-    print("USER:", os.getenv("USER_DB"))
-    
+    credentials = get_secrets()
+
     conn = psycopg2.connect(
-      host= os.getenv("DB_HOST"),
-      database= os.getenv("DATABASE"),
-      user= os.getenv("USER_DB"),
-      password= os.getenv("PASSWORD")
+      host = credentials['host'],
+      database = credentials['database'],
+      user = credentials['username'],
+      password = credentials['password'],
     )
+
     return conn
 
   except psycopg2.OperationalError as e:
     print("Error al conectar con la db", e)
+    return None
+  
+  except Exception as e:
+    print("Error inesperado", e)
     return None
  
